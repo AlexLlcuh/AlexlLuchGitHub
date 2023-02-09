3042adf1f5bbcc120083c874d5a9511dfea539bf
AlexLlcuh <97671787+AlexLlcuh@users.noreply.github.com>
2023-02-09 09:50:14 +0100

    Comit Script matriculas
    Crea un script que calcule cuantas matriculas hay

---
+++ b.sh
@@ -0,0 +1,7 @@
+file=file "Matriculas.txt"
+
+if [ -f "$file" ]; then
+  num_Matriculas=$(wc -l < "$file")
+  echo "Hay $num_Matriculas matrículas en el archivo $file"
+else
+  echo "El archivo $file no existe."
