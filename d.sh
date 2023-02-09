7a9429bf1150192018b71ee72bd663f8e605c402
AlexLlcuh <97671787+AlexLlcuh@users.noreply.github.com>
2023-02-09 10:11:41 +0100

    error
    error en Notas

--- d.sh
+++ d.sh
@@ -4,9 +4,9 @@
 if [ "$#" -eq 1 ]; then
   Nota=$1
 
-  if [ "$Nota" -lt 5 ]; then
+  if [ "$nota" -lt 5 ]; then
     echo "Suspendido"
-  elif [ "$Nota" -ge 5 ] && [ "$Nota" -lt 8 ]; then
+  elif [ "$nota" -ge 5 ] && [ "$Nota" -lt 8 ]; then
     echo "Aprobado"
   else
     echo "Sobresaliente"
